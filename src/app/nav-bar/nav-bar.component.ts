4d527d118aa68f9ba03c741344fb75711996b9df
Vinicius Martins <visimonemartins@hotmail.com>
2019-12-15 21:04:30 -0300

    Update nav-bar with logout button

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+  public isLogged: boolean = null;
 
   constructor() { }
 
   ngOnInit() {
+    this.checkLoggedUser();
+  }
+
+  private checkLoggedUser(): void {
+    const user = this.getUser();
+
+    // @ts-ignore
+    if (!Object.keys(user).length) {
+      this.isLogged = false;
+      return;
+    }
+
+    this.isLogged = true;
+  }
+
+
+  private getUser(): void {
+    return JSON.parse(localStorage.getItem('_auth_info'));
   }
 
 }
